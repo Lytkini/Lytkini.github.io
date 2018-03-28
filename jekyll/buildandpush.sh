4a6d3d4a1a31d9921ecc404d68e6bab0956925f9
Lytkini <ilytkin88@gmail.com>
2018-03-28 17:20:42 +0300

    updt

--- jekyll/buildandpush.sh
+++ jekyll/buildandpush.sh
@@ -1,12 +1,6 @@
 echo "Build English site..."
 bundle exec jekyll build --destination outputs/;
 echo "Done"
-echo "Building Japanese site ..."
-bundle exec jekyll build --config _config_ja.yml --destination outputs/ja/;
-echo "Done"
-echo "Building German site ..."
-bundle exec jekyll build --config _config_de.yml --destination outputs/de/;
-echo "Done"
 echo "Applying any changes to s3 config file..."
 s3_website cfg apply;
 echo "Done"
